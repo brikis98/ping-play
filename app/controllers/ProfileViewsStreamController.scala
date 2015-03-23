9d42d75728fe25b0bd97c496e93661e8589327f1
Ahmed Obaidi <ahmad.alobaidy@gmail.com>
2015-03-22 18:53:21 -0700

    Composite Page with MVC

---
+++ app/controllers/ProfileViewsStreamController.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import data.ServiceClient
+import play.api.libs.concurrent.Execution.Implicits._
+import ui.HtmlStreamImplicits._
+import ui.{HtmlStream, Pagelet}
+
+/**
+ * An example standalone module that represents the "Who's Viewed Your Profile" module from the LinkedIn homepage.
+ * It's identical to the ProfileViewsController controller, except that the data is streamed to the browser as soon as it's available in
+ * small "pagelets", similar to Facebook's BigPipe.
+ */
+object ProfileViewsStreamController {
+
+  def stream = HtmlStream {
+
+    val wvypCountFuture = ServiceClient.makeServiceCall("wvyp")
+    val searchCountFuture = ServiceClient.makeServiceCall("search")
+
+    val wvypStream = Pagelet.renderStream(wvypCountFuture.map(str => views.html.pagelets.profiles.wvypCount(str.toInt)), "wvypCount")
+    val searchStream = Pagelet.renderStream(searchCountFuture.map(str => views.html.pagelets.profiles.searchCount(str.toInt)), "searchCount")
+    HtmlStream.interleave(wvypStream, searchStream)
+  }
+
+}
---
+++ app/controllers/UpdateViewsStreamController.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import data.ServiceClient
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.mvc.Action
+import ui.HtmlStreamImplicits._
+import ui.{HtmlStream, Pagelet}
+
+/**
+ * An example standalone module that represents the "Who's Viewed Your Profile" module from the LinkedIn homepage.
+ * It's identical to the ProfileViewsController controller, except that the data is streamed to the browser as soon as it's available in
+ * small "pagelets", similar to Facebook's BigPipe.
+ */
+object UpdateViewsStreamController {
+
+  def stream = HtmlStream {
+
+    val likesCountFuture = ServiceClient.makeServiceCall("likes")
+    val commentsCountFuture = ServiceClient.makeServiceCall("comments")
+
+
+    val likesStream =Pagelet.renderStream(likesCountFuture.map(str => views.html.pagelets.updates.likesCount(str.toInt)), "likeCount")
+    HtmlStream.interleave(likesStream)
+  }
+}
